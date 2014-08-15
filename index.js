74bad535c8e878dbd838ccae907650d03018c6b8
JoelLaCroix <joel.lacroix1@gmail.com>
2014-08-15 17:21:57 -0400

    more chat updates
    added in some more checks to the chat when user enters name.
    Experimenting with dynamically creating rooms when a user connects and
    limiting the connections to a room to just two users

--- index.js
+++ index.js
@@ -4,40 +4,68 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
 
+//io.sockets.clients('room') - returns number of clients in room
 
 app.use(express.static(__dirname + '/public'));
 
 
-//app.get('/', function(req, res){
-//res.sendFile(path.resolve('public/index.html'));
-//res.sendFile(__dirname +'/public/index.html');
-
-//});
-
 io.on('connection',function(socket){
-/**
- *console.log('a user connected');
- */
- 
- 
+ console.log( 'current socket connected: ' + socket.id); // checking connection
   // player chat
+  // when server recieves the player ready message
 	socket.on('player ready',function (player){
-	 io.emit('player ready', player);
-	 console.log( player +' is ready');
-		socket.on('chat message', function(msg){
-		io.emit('chat message', {Player: player, Msg: msg});
-		console.log( player +' : ' + msg);
-		});
-	 });
-	 
-
 	
-	/**
-	 *socket.on('disconnect',function(){
-	 *console.log('user disconnected');
-	 *});
-	 */
+		// check to see if the player name is empty
+		if(player.length <1)
+		{
+			console.log('empty player name');
+		}
+		
+		// else returns to the client chat window that player is ready
+		else
+		{
+			//socket.on('create room', function (room){
+			//socket.join(room);
+			//console.log(socket.rooms);
+			/**
+			 *if (io.sockets.manager.rooms.indexOf(room) < 0)
+			 *{
+			 *	socket.join(room);
+			 *}
+			 */
 
+			
+			//if (true)
+			//{
+					io.emit('player ready', player);
+					console.log( player +' is ready'); // logs player name
+					
+					// when server recieves chat message, message
+					socket.on('chat message', function(msg){
+					
+						// if the length od the message array is less then 1, meaning empty message
+						if (msg.length < 1)
+						{
+							console.log( 'empty message'); // logs message 
+						}
+						
+						// else return player name and message to the client
+						else
+						{
+							io.emit('chat message', {Player: player, Msg: msg});
+							console.log( player +' : ' + msg); // logs message
+						}
+						
+						// when server recieves disconnect message, return to client that the user has disconnected
+						socket.on('disconnect',function(){
+						io.emit('disconnect', {Player: player, Msg: "has disconnected"});
+						console.log( player + ': has disconnected');
+						});
+					});
+				//}
+			//});
+		}
+	});
 });
 
 
